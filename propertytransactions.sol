96dc5a1e9e9ae6e5fb40fd21908fb0ea56f190e3
peternderitu <31569127+peternderitu@users.noreply.github.com>
2020-07-29 12:22:51 +0300

    Update propertytransactions.sol

--- propertytransactions.sol
+++ propertytransactions.sol
@@ -1,8 +1,18 @@
+pragma solidity ^0.6.0;
+
+import './propertyownership.sol';
+
 contract PropertyTransactions is PropertyOwnership {
     
-    uint stampduty = 0.04 * value;
+    uint stampduty = 5;
+    uint leasePeriod;
+    uint leaseFee;
+    uint value;
+    
+    mapping (uint => address) subdivisionAllowedToAddress;
+    
     modifier onlyValidator() {
-        require (msg.sender == validator);
+        require (msg.sender == validatorDetails[addr].addr);
         _;
     }
    // function withdraw() external onlyOwner {
@@ -20,15 +30,15 @@ contract PropertyTransactions is PropertyOwnership {
     function _valuateProperty(uint _value) onlyValidator() internal {
        value = _value; 
     }
-    function leaseProperty(uint _propertyId) external payable {
+    function leaseProperty(uint _propertyId) public payable {
         require(msg.value == leaseFee);
         if (leasePeriod > 0){
-        transferFrom(holder, msg.sender, _propertyId);
+        transferFrom(owner(), msg.sender, _propertyId);
         } else {
             renewLease(_propertyId);
         }
     }
-    function renewLease(uint _propertyId) external {
+    function renewLease(uint _propertyId) public {
         leaseProperty(_propertyId);
     }
     function payTax(uint _tax) external payable onlyOwner {

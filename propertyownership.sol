b3e1d12e9c75a55c5d39c69b7f9c105a889d37c5
peternderitu <31569127+peternderitu@users.noreply.github.com>
2020-07-29 12:19:56 +0300

    Update propertyownership.sol

--- propertyownership.sol
+++ propertyownership.sol
@@ -5,32 +5,44 @@ import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract PropertyOwnership is LandReg, ERC721 {
     
+    constructor() ERC721("TitleDeed", "TD") public { }
+    
     mapping (uint => address) propertyApprovals;
+    
     modifier onlyOwnerOf(uint _propertyId) {
     require(msg.sender == propertyToHolder[_propertyId]);
     _;
   }
-    
-    function balanceOf(address _holder) external view returns(uint) {
+    function _owns(address _claimingaddr, uint _tokenId) internal view returns(bool) {
+        return propertyToHolder[_tokenId] == _claimingaddr;
+    }
+    function BalanceOf(address _holder) public view returns(uint count) {
         return holderToPropertyCount[_holder];
     }
-    function ownerOf(uint _tokenId) external view returns(address){
+    function OwnerOf(uint _tokenId) public view  returns(address){
         return propertyToHolder[_tokenId];
     }
-    function _transfer(address _from, address _to, uint256 _tokenId) private {
+    function TotalSupply() public view returns (uint) {
+        return properties.length - 1;
+    }
+
+    function _transfer(address _from, address _to, uint256 _tokenId) internal override {
     holderToPropertyCount[_to] = holderToPropertyCount[_to].add(1);
     holderToPropertyCount[msg.sender] = holderToPropertyCount[msg.sender].sub(1);
     propertyToHolder[_tokenId] = _to;
     emit Transfer(_from, _to, _tokenId);
   }
 
-  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+    function transferFrom(address _from, address _to, uint256 _tokenId) public override {
       require (propertyToHolder[_tokenId] == msg.sender || propertyApprovals[_tokenId] == msg.sender);
       _transfer(_from, _to, _tokenId);
     }
 
-  function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
+    function approve(address _approved, uint256 _tokenId) public override onlyOwnerOf(_tokenId) {
       propertyApprovals[_tokenId] = _approved;
       emit Approval(msg.sender, _approved, _tokenId);
+    }
+    function _approvedFor(address _claimingaddr, uint _tokenId) internal view returns(bool) {
+        return propertyApprovals[_tokenId] == _claimingaddr;
     }
 }

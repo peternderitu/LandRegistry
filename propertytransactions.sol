4d175d8a6d0434ba05e6f7c3487041ea8bddd7bf
peternderitu <31569127+peternderitu@users.noreply.github.com>
2020-08-04 01:30:17 +0300

    Update propertytransactions.sol

--- propertytransactions.sol
+++ propertytransactions.sol
@@ -3,30 +3,37 @@ pragma solidity ^0.6.0;
 import './propertyownership.sol';
 
 contract PropertyTransactions is PropertyOwnership {
-    
+    // struct that contains properties that are to be leased or rented
     struct rentedProperty {
+        //original owner and lender of the property
         address payable lessor;
+        // the renter
         address lessee;
         uint tokenId;
         bool isLeased;
         uint startLease;
         uint endLease;
-        uint leasePeriod;
+        uint leasePeriodinseconds;
         uint leaseFee;
     }
+    // value of tax on property to be paid to the government
+    uint stampduty = 5 * value;
     
-    uint stampduty = 5;
+    //property value or cost of a property
     uint value;
+    
+
     mapping(uint => rentedProperty) public propertyAvailableToLease;
-    mapping (uint => address) subdivisionAllowedToAddress;
+    mapping (uint => uint) propertyIdToValue;
     
     modifier onlyValidator(address addr) {
         require (msg.sender == validatorDetails[addr].addr);
         _;
     }
   
-   function enablePropertyForLeasing(uint _propertyId, uint _leasePeriod, uint _leaseFee) public onlyOwner returns(bool) {
-       require(_leasePeriod > 0, "Must have leasePeriod");
+  
+   function enablePropertyForLeasing(uint _propertyId, uint _leasePeriodinseconds, uint _leaseFee) public onlyOwner returns(bool) {
+       require(_leasePeriodinseconds > 0, "Must have leasePeriod");
        require(_leaseFee > 0, "Must have leaseFee");
        
        propertyAvailableToLease[_propertyId] = rentedProperty({
@@ -36,16 +43,18 @@ contract PropertyTransactions is PropertyOwnership {
            isLeased: false,
            startLease: 0,
            endLease:0,
-           leasePeriod: _leasePeriod,
+           leasePeriodinseconds: _leasePeriodinseconds,
            leaseFee: _leaseFee
        });
        
        return true;
    }
     
-    function _valuateProperty(uint _value) onlyValidator(msg.sender) internal {
-       value = _value; 
+    function _valuateProperty(uint _propertyId, uint _value) onlyValidator(msg.sender) internal {
+        propertyIdToValue[_propertyId] = _value;
+        _value = value;
     }
+    
     function leaseProperty(uint _propertyId) public payable returns(bool) {
         rentedProperty storage rentP = propertyAvailableToLease[_propertyId];
         require(msg.value == rentP.leaseFee);
@@ -56,7 +65,7 @@ contract PropertyTransactions is PropertyOwnership {
         rentP.isLeased = true;
         
         rentP.startLease = now;
-        rentP.endLease = now + rentP.leasePeriod;
+        rentP.endLease = now + rentP.leasePeriodinseconds;
         
         transferFrom(msg.sender, address(this), rentP.leaseFee);
         transferFrom(rentP.lessor, msg.sender, _propertyId);
@@ -70,6 +79,8 @@ contract PropertyTransactions is PropertyOwnership {
         if (block.timestamp <= rentP.startLease) return rentP.endLease - rentP.startLease;
         if (block.timestamp > rentP.endLease) return 0;
     }
+    
+    // returns ownership of lease to lender
     function getBackProperty(uint _propertyId) public {
         uint timeLeft = getRemainingTimeLeftForLease(_propertyId);
         
@@ -82,8 +93,8 @@ contract PropertyTransactions is PropertyOwnership {
     
     function payTax() external payable onlyOwner {
         require(msg.value == stampduty);
-        
     }
+    
     function withdraw(uint _propertyId) external onlyOwner {
         rentedProperty storage rentP = propertyAvailableToLease[_propertyId];
         address payable _owner = rentP.lessor;

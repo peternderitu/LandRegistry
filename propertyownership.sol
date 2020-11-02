42a295a51618abc88f35f662384659e583ae45cd
peternderitu <31569127+peternderitu@users.noreply.github.com>
2020-11-02 23:13:00 +0300

    Update propertyownership.sol

--- propertyownership.sol
+++ propertyownership.sol
@@ -7,7 +7,8 @@ contract PropertyOwnership is LandReg, ERC721 {
     
     constructor() ERC721("TitleDeed", "TD") public { }
     
-    mapping (uint => address) propertyApprovals;
+    mapping (address => uint) public propertyApprovals;
+    mapping (address => uint) public ownerToToken;
     
     
     modifier onlyOwnerOf(uint _propertyId) {
@@ -18,36 +19,19 @@ contract PropertyOwnership is LandReg, ERC721 {
         require (msg.sender == validatorDetails[addr].addr);
         _;
     }
-    function _owns(address _claimingaddr, uint _tokenId) internal view returns(bool) {
-        return propertyToHolder[_tokenId] == _claimingaddr;
+    function approveTokenization(address holder) internal onlyValidator(msg.sender) returns(uint){
+        uint tokenid = properties.length -1;
+        propertyApprovals[holder]+=tokenid;
+        return propertyApprovals[holder];
     }
-    function BalanceOf(address _holder) public view returns(uint count) {
-        return holderToPropertyCount[_holder];
+    function mintToken(address owner, uint token) internal {
+        _safeMint(owner,token);
     }
-    function OwnerOf(uint _tokenId) public view  returns(address){
-        return propertyToHolder[_tokenId];
+    function createTitle() public returns(uint){
+        uint tokenId = approveTokenization(msg.sender);
+        mintToken(msg.sender,tokenId);
+        ownerToToken[msg.sender]+=tokenId;
+        return totalSupply();
     }
-    function TotalSupply() public view returns (uint) {
-        return properties.length - 1;
-    }
-
-    function _transfer(address _from, address _to, uint256 _tokenId) internal override {
-    holderToPropertyCount[_to] = holderToPropertyCount[_to].add(1);
-    holderToPropertyCount[msg.sender] = holderToPropertyCount[msg.sender].sub(1);
-    propertyToHolder[_tokenId] = _to;
-    emit Transfer(_from, _to, _tokenId);
-  }
-
-    function transferFrom(address _from, address _to, uint256 _tokenId) public override {
-      require (propertyToHolder[_tokenId] == msg.sender || propertyApprovals[_tokenId] == msg.sender);
-      _transfer(_from, _to, _tokenId);
-    }
-
-    function approve(address _approved, uint256 _tokenId) public override onlyOwnerOf(_tokenId) onlyValidator(msg.sender) {
-      propertyApprovals[_tokenId] = _approved;
-      emit Approval(msg.sender, _approved, _tokenId);
-    }
-    function _approvedFor(address _claimingaddr, uint _tokenId) internal view returns(bool) {
-        return propertyApprovals[_tokenId] == _claimingaddr;
-    }
+    
 }
